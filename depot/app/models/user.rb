9ca3510dcdc285d9cd8bb60dc247e74e30240511
Paresh <paresh@sahil-Vostro-3560.(none)>
2014-09-11 17:53:24 +0530

    solve indentation

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_secure_password
@@ -17,5 +9,5 @@ class User < ActiveRecord::Base
       if User.count.zero?
         raise "Can't delete last user"
       end
-    end     
+    end
 end
