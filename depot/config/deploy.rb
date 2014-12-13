6e522065f0883071b3422dd43581c3e476bdce44
PareshGupta <paresh@vinsol.com>
2014-12-13 14:01:51 +0530

    add unicorn config file

--- depot/config/deploy.rb
+++ depot/config/deploy.rb
@@ -1,8 +1,92 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'depot'
+set :repo_url, 'https://github.com/PareshGupta/DepotApp.git'
+set :branch, 'master'
+set :scm, :git
+set :deploy_to, '/var/www/depot/'
+set :sudo, false
+
+# Default value for :linked_files is []
+set :linked_files, %w(config/database.yml config/secrets.yml)
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets)
+
+namespace :deploy do
+  after :publishing, :restart
+
+  after :restart, :unicorn_restart do
+    on roles(:web), in: :parallel do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          Rake::Task[:'unicorn:hard_restart'].invoke
+        end
+      end
+    end
+  end
+end
+
+namespace :unicorn do
+  task :hard_restart do
+    Rake::Task[:'unicorn:stop'].invoke
+    Rake::Task[:'unicorn:start'].invoke
+  end
+
+  desc 'start unicorn'
+  task :start do
+    on roles(:app), in: :parallel do
+      within current_path do
+        execute :bundle, :exec, "unicorn_rails -c config/unicorn.rb -D -E #{ fetch(:rails_env) }"
+      end
+    end
+  end
+
+  desc 'stop unicorn'
+  task :stop do
+    on roles(:app), in: :parallel do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, :exec, "kill -s QUIT `cat #{shared_path}/tmp/pids/unicorn.pid`"
+        end
+      end
+    end
+  end
+
+  desc 'restart unicorn'
+  task :restart do
+    on roles(:app), in: :parallel do
+      within current_path do
+        execute "kill -s USR2 `cat #{shared_path}/tmp/pids/unicorn.pid`"
+      end
+    end
+  end
+end
+
+namespace :database do
+  desc 'create database'
+  task :create do
+    on roles(:db), in: :parallel do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'seed'
+  task :seed do
+    on roles(:db), in: :parallel do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
