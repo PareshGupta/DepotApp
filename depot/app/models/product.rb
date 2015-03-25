84ed02567fcb21dd4994b3efd92e6bfdd31f6345
PareshGupta <paresh@vinsol.com>
2015-03-25 15:59:25 +0530

    minor fixes

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -8,10 +8,10 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with:    %r{ \.(gif|jpg|png)\Z }i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
-  }
+  # validates :image_url, allow_blank: true, format: {
+  #   with:    %r{ \.(gif|jpg|png)\Z }i,
+  #   message: 'must be a URL for GIF, JPG or PNG image.'
+  # }
   validates :title, length: {minimum: 10}
 
   def self.latest
